c57ebb4181161046ef5508c0804a062d6ca3fff5
XinyuLoveAo <455001000@qq.com>
2020-11-23 10:00:06 +0800

    完成晚签到

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ from flask import Flask,render_template,request
 import sys
 import requests
 import json
+import uuid
 import yaml
 import oss2
 from urllib.parse import urlparse
 from datetime import datetime, timedelta, timezone
 from urllib3.exceptions import InsecureRequestWarning
+import base64
+import requests
+from pyDes import des, CBC, PAD_PKCS5
 
 app = Flask(__name__)
 
@@ -17,7 +21,6 @@ def getYmlConfig(yaml_file='config.yml'):
     file.close()
     config = yaml.load(file_data, Loader=yaml.FullLoader)
     return dict(config)
-
 
 # 全局配置
 config = getYmlConfig(yaml_file='config.yml')
@@ -154,29 +157,136 @@ def submitForm(formWid, collectWid, schoolTaskWid, form, session):
     msg = r.json()['message']
     return msg
 
+
+def getUnSignedTasks(session):
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'content-type': 'application/json',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+    # 第一次请求每日签到任务接口，主要是为了获取MOD_AUTH_CAS
+    res = session.post(
+        url='https://cumt.campusphere.net/wec-counselor-sign-apps/stu/sign/queryDailySginTasks',
+        headers=headers, data=json.dumps({}))
+    # 第二次请求每日签到任务接口，拿到具体的签到任务
+    res = session.post(
+        url='https://cumt.campusphere.net/wec-counselor-sign-apps/stu/sign/queryDailySginTasks',
+        headers=headers, data=json.dumps({}))
+    if len(res.json()['datas']['unSignedTasks']) < 1:
+        return None
+    # log(res.json())
+    latestTask = res.json()['datas']['unSignedTasks'][0]
+    return {
+        'signInstanceWid': latestTask['signInstanceWid'],
+        'signWid': latestTask['signWid']
+    }
+
+
+# 获取签到任务详情
+def getDetailTask(session, params):
+    headers = {
+        'Accept': 'application/json, text/plain, */*',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36',
+        'content-type': 'application/json',
+        'Accept-Encoding': 'gzip,deflate',
+        'Accept-Language': 'zh-CN,en-US;q=0.8',
+        'Content-Type': 'application/json;charset=UTF-8'
+    }
+    res = session.post(
+        url='https://cumt.campusphere.net/wec-counselor-sign-apps/stu/sign/detailSignTaskInst',
+        headers=headers, data=json.dumps(params))
+    data = res.json()['datas']
+    # log(data)
+    return data
+
+# 填充表单
+def fillsignForm(task, session,user):
+    form = {}
+    form['signInstanceWid'] = task['signInstanceWid']
+    form['longitude'] = '117.151750'
+    form['latitude'] = '34.220392'
+    return form
+
+# DES加密
+def DESEncrypt(s, key='ST83=@XV'):
+    key = key
+    iv = b"\x01\x02\x03\x04\x05\x06\x07\x08"
+    k = des(key, CBC, iv, pad=None, padmode=PAD_PKCS5)
+    encrypt_str = k.encrypt(s)
+    return base64.b64encode(encrypt_str).decode()
+
+# 提交签到任务
+def signForm(session, user, form):
+    # Cpdaily-Extension
+    extension = {
+        "lon": '117.151750',
+        "model": "OPPO R11 Plus",
+        "appVersion": "8.1.14",
+        "systemVersion": "4.4.4",
+        "userId": user,
+        "systemName": "android",
+        "lat": '34.220392',
+        "deviceId": str(uuid.uuid1())
+    }
+
+    headers = {
+        # 'tenantId': '1019318364515869',
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.4; OPPO R11 Plus Build/KTU84P) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/33.0.0.0 Safari/537.36 okhttp/3.12.4',
+        'CpdailyStandAlone': '0',
+        'extension': '1',
+        'Cpdaily-Extension': DESEncrypt(json.dumps(extension)),
+        'Content-Type': 'application/json; charset=utf-8',
+        'Accept-Encoding': 'gzip',
+        # 'Host': 'swu.cpdaily.com',
+        'Connection': 'Keep-Alive'
+    }
+    res = session.post(url='https://cumt.campusphere.net/wec-counselor-sign-apps/stu/sign/completeSignIn',
+                       headers=headers, data=json.dumps(form))
+    msg = res.json()['message']
+    return msg
+
+# 日报告
 @app.route('/submit/<username>/<password>', methods=['POST'])
-def tst(username,password):
+def submit(username,password):
     session = getSession(str(username),str(password))
     if session != None:
         # return  "模拟登陆成功"
         params = queryForm(session)
         if str(params) == 'None':
             return "获取最新待填写问卷失败，可能是辅导员还没有发布"
-            # exit(-1)
-        form = fillForm(session, params['form'])
-        # return "填写问卷成功"
-        msg = submitForm(params['formWid'], params['collectWid'],
-                            params['schoolTaskWid'], form, session)
-        if msg == 'SUCCESS':
-            return "自动提交成功"
-        elif msg == '该收集已填写无需再次填写':
-            return "今日已提交"
         else:
-            return "自动提交失败" + msg
-            #exit(-1)
+            form = fillForm(session, params['form'])
+            msg = submitForm(params['formWid'], params['collectWid'],
+                                params['schoolTaskWid'], form, session)
+            if msg == 'SUCCESS':
+                return "自动提交成功"
+            elif msg == '该收集已填写无需再次填写':
+                return "今日已提交"
+            else:
+                return "自动提交失败" + msg
     else:
         return "模拟登陆失败"
-        #exit(-1)
+
+# 晚签到
+@app.route('/sign/<username>/<password>', methods=['POST'])
+def sign(username,password):
+    session = getSession(str(username),str(password))
+    if session != None:
+        params = getUnSignedTasks(session)
+        task = getDetailTask(session, params)
+        form = fillsignForm(task, session, username)
+        msg = signForm(session, username, form)
+        if msg == 'SUCCESS':
+            return "自动提交成功"
+        elif msg == '任务未开始，扫码签到无效！':
+            return "未到签到时间"        
+        else:
+            return "自动提交失败," + msg 
+    else:
+        return('模拟登陆失败')
 
 if __name__ == '__main__':
     # Flask
